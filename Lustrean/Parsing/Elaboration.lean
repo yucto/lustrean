1a19455253934abb1c31f91f1cc32cf40cca0f4b
jthulhu <adrien.lc.mathieu@gmail.com>
2024-12-08 21:05:44 +0100

    [Lustrean/Parsing/Elaboration] Added driver code for the parsing, and cleanup.

--- Lustrean/Parsing/Elaboration.lean
+++ Lustrean/Parsing/Elaboration.lean
@@ -1,17 +1,29 @@
-import Lustrean.Parsing.Phase2
+import Lustrean.Parsing.Reify
+import Lustrean.Parsing.Inline
+import Lustrean.Parsing.Indicise
 
-open Lean.Elab.Command (liftTermElabM)
+open Lean
+open Elab (liftMacroM)
+open Elab.Command (liftTermElabM)
+open Core (CoreM)
 
 namespace Lustrean.Parsing
 
+def elab_lustre (nodes : TSyntaxArray `lustre_node) : CoreM Unit := do
+  let nodes ← Indicise.elab_lustre <|
+    ← Inline.elab_lustre <|
+    ← Reify.elab_lustre <|
+    nodes
+  for nod in nodes do
+    println! s!"{nod.value}\n"
+
 elab_rules : command
-  | `(command| lustre $nodes:lustre_node*) =>
-    for nod in nodes do
-      let nod1 ← liftTermElabM <| Phase1.elab_node nod
-      let _nod2 ← liftTermElabM <| Phase2.elab_node nod1
+  | `(command| lustre $nodes:lustre_node*) => do
+    let nodes ← nodes.mapM fun nod => do
+      let nod ← liftMacroM <| expandMacros nod.raw
+      return .mk nod
+    liftTermElabM <| elab_lustre nodes
 
 lustre
-  node hello(x : nat, y : nat) = o where
-    o : nat = x + y
 
 end Lustrean.Parsing
