b60d8a455161b6444b1b14af84251382e2c14fe5
jthulhu <adrien.lc.mathieu@gmail.com>
2025-01-06 07:14:22 +0100

    [Elaboration/Compile] Added loop unrolling.

--- Lustrean/Elaboration/Compile.lean
+++ Lustrean/Elaboration/Compile.lean
@@ -42,15 +42,87 @@ section
       | .and => .and l r
 end
 
+/-- How many iterations of the main loop to unroll. -/
+def unroll_loop : Nat := 1
+
 def elab_into_cfg (nod : Normalize.Node) : List (PreNode nod.total_vars) × Array (&Fin nod.total_vars) := Id.run do
   -- TODO: default is a dummy value
-  let mut result := #[{ id := 0, out_nodes := [(1, .assign step (IExpr.const 0), default)] }]
+  let mut result := #[
+    { id := 0, out_nodes := [(2, .assign step (IExpr.const 0), default)] },
+    { id := 1, out_nodes := [] }
+  ]
   for h : i in [0:nod.m] do
     let k := Fin.mk i <| Membership.get_elem_helper h rfl
     result := result.push {
       id := result.size
       -- TODO: default is a dummy value
       out_nodes := [(result.size + 1, .assign (bound_var k) .nil, default)]
+    }
+  for _ in [0:unroll_loop] do
+    for h : i in [0:nod.m] do
+      let k := Fin.mk i <| Membership.get_elem_helper h rfl
+      result := result.push {
+        id := result.size
+        -- TODO: default is a dummy value
+        out_nodes := [(result.size + 1, .assign (old_bound_var k) (.var <| bound_var k), default)]
+      }
+    for h : i in [0:nod.n] do
+      let k := Fin.mk i <| Membership.get_elem_helper h rfl
+      result := result.push {
+        id := result.size
+        -- TODO: default is a dummy value
+        out_nodes := [(result.size + 1, .assign (input_var k) (.rand none none), default)]
+      }
+    for g in nod.guards do
+      result := result.push {
+        id := result.size
+        -- TODO: default is a dummy value
+        out_nodes := [(result.size + 1, .guard g.to_cfg_expr, default)]
+      }
+    for h : i in [0:nod.m] do
+      let k := Fin.mk i <| Membership.get_elem_helper h rfl
+      result := result.push {
+        id := result.size
+        -- TODO: default is a dummy value
+        out_nodes := [(result.size + 1, .assign (bound_var k) .nil, default)]
+      }
+    let there_id := result.size
+    for h : i in [0:nod.m] do
+      let k := Fin.mk i <| Membership.get_elem_helper h rfl
+      match nod.bound_vars[i].value with
+      | .simple e =>
+        result := result.push {
+          id := result.size
+          -- TODO: default is a dummy value
+          out_nodes := [(result.size + 1, .assign (bound_var k) e.to_cfg_expr, default)]
+        }
+      | .ite cond e₁ e₂ =>
+        result := result.push {
+          id := result.size
+          -- TODO: default are dummy values
+          out_nodes := [
+            (result.size + 1, .guard cond.to_cfg_expr, default),
+            (result.size + 2, .guard cond.to_cfg_expr.not, default)
+          ]
+        }
+        result := result.push {
+          id := result.size
+          -- TODO: default is a dummy value
+          out_nodes := [(result.size+2, .assign (bound_var k) e₁.to_cfg_expr, default)]
+        }
+        result := result.push {
+          id := result.size
+          out_nodes := [(result.size+1, .assign (bound_var k) e₂.to_cfg_expr, default)]
+        }
+    result := result.push {
+      id := result.size
+      -- TODO: default are dummy values
+      out_nodes := [
+        (there_id, .skip, default),                   -- loop again current iteration
+        (1, .skip, default),                          -- exit program
+        -- next iteration
+        (result.size + 1, .assign step (.binop (.var step) .iadd (IExpr.const 1)), default)
+      ]
     }
   let here_id := result.size
   for h : i in [0:nod.m] do
@@ -122,10 +194,17 @@ def elab_into_cfg (nod : Normalize.Node) : List (PreNode nod.total_vars) × Arra
       id := result.size
       out_nodes := [(result.size+1, .assert a.value.to_cfg_expr, a.ref)]
     }
+  let exit_id := result.size
   result := result.push {
-    id := result.size
+    id := exit_id
     out_nodes := []
   }
+  result := { result with [1] := {
+    id := 1
+    out_nodes := [
+      (exit_id, .skip, default)
+    ]
+  }}
   let output_vars := nod.output_vars.map (·.map fun
     | .step => step
     | .input_var k => input_var k
