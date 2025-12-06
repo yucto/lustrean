b5088ac5c1f3b07d7603d69fae93afeb078c8273
arthur-adjedj <arthur.adjedj@ens-paris-saclay.fr>
2025-12-06 16:09:50 +0100

    chore: bump to v4.25.0 and add `LeanSearchclient` as a dependency

--- lakefile.lean
+++ lakefile.lean
@@ -1,7 +1,8 @@
 import Lake
 open Lake DSL
 
-require batteries from git "https://github.com/leanprover-community/batteries" @ "v4.24.0"
+require "leanprover-community" / "batteries" @ git "v4.25.0"
+require "leanprover-community" / "LeanSearchClient" @ git "3591c3f664ac3719c4c86e4483e21e228707bfa2"
 
 package "lustrean" where
   version := v!"0.1.0"
