399feb3f5a60d2c0d6c2f499c952b5bdb66312fc
ayhon <ayhonnick@gmail.com>
2025-12-09 22:06:13 +0100

    fix: `add` implementation + correctness proof

--- Lustrean/Domain/Sign.lean
+++ Lustrean/Domain/Sign.lean
@@ -3,6 +3,15 @@ import Lustrean.Domain.GaloisConnection
 
 namespace Lustrean.Domain
 
+attribute [local simp] Int.compare_eq_gt Int.compare_eq_lt
+private def Int.compare_eq_of_lt a b := @Int.compare_eq_lt a b  |>.mpr
+local grind_pattern Int.compare_eq_of_lt => compare a b
+private def Int.compare_eq_of_eq a b := @Int.compare_eq_eq a b  |>.mpr
+local grind_pattern Int.compare_eq_of_eq => compare a b
+private def Int.compare_eq_of_gt a b := @Int.compare_eq_gt a b  |>.mpr
+local grind_pattern Int.compare_eq_of_gt => compare a b
+
+
 /-- Abstraction over sets of integers. The only
  information retained is the sign of the elements
  of the set.  -/
@@ -14,79 +23,101 @@ structure Sign where mk ::
 
 namespace Sign
 /-- Seeing elements in `Sign` as a set, the opposite -/
-  def opposite(s: Sign): Sign where
-    hasZero := ! s.hasZero
-    hasPos := ! s.hasPos
-    hasNeg := ! s.hasNeg
+def opposite(s: Sign): Sign where
+  hasZero := ! s.hasZero
+  hasPos := ! s.hasPos
+  hasNeg := ! s.hasNeg
 
 /-  We name all elements of the type.  -/
 section elements
-  def None: Sign := {}
-  def Zero: Sign := {hasZero := true}
-  def Pos: Sign := {hasPos := true}
-  def Neg: Sign := {hasNeg := true}
-  def NonZero: Sign := Zero.opposite
-  def ZeroPos: Sign := Neg.opposite
-  def ZeroNeg: Sign := Pos.opposite
-  def All: Sign := None.opposite
+def None: Sign := {}
+def Zero: Sign := {hasZero := true}
+def Pos: Sign := {hasPos := true}
+def Neg: Sign := {hasNeg := true}
+def NonZero: Sign := Zero.opposite
+def ZeroPos: Sign := Neg.opposite
+def ZeroNeg: Sign := Pos.opposite
+def All: Sign := None.opposite
 end elements
 
-  instance: ToString Sign where toString := fun
-  |.mk false false false => "[⊥]"
-  |.mk true  false false => "[=0]"
-  |.mk false true  false => "[>0]"
-  |.mk false false true  => "[<0]"
-  |.mk true  true  false => "[≥0]"
-  |.mk true  false true  => "[≤0]"
-  |.mk false true  true  => "[≠0]"
-  |.mk true  true  true  => "[⊤]"
+instance: Std.ToFormat Sign where format := fun
+|.mk false false false => "[⊥]"
+|.mk true  false false => "[=0]"
+|.mk false true  false => "[>0]"
+|.mk false false true  => "[<0]"
+|.mk true  true  false => "[≥0]"
+|.mk true  false true  => "[≤0]"
+|.mk false true  true  => "[≠0]"
+|.mk true  true  true  => "[⊤]"
+instance: ToString Sign := ⟨toString ∘ Std.format⟩
+instance: Repr Sign := ⟨fun a _ => Std.format a⟩
 
-  def add(a b : Sign): Sign where
-    hasZero := a.hasZero || b.hasZero
-    hasPos  := a.hasPos || b.hasPos
-    hasNeg  := a.hasNeg || b.hasNeg
+namespace Notation
+scoped notation "[⊥]" => Sign.None
+scoped notation "[⊤]" => Sign.All
+scoped notation "[=0]" => Sign.Zero
+scoped notation "[>0]" => Sign.Pos
+scoped notation "[<0]" => Sign.Neg
+scoped notation "[≥0]" => Sign.ZeroPos
+scoped notation "[≤0]" => Sign.ZeroNeg
+end Notation
 
-  def mul(a b : Sign): Sign :=
-    if a = .Zero ∨ b = .Zero then
-      .Zero
-    else
-      {
-        hasZero := a.hasZero || b.hasZero,
-        hasPos  := a.hasPos && b.hasPos || a.hasNeg && b.hasNeg
-        hasNeg  := a.hasNeg && b.hasPos || b.hasNeg && b.hasPos
-      }
+def join(a b: Sign): Sign where
+  hasZero := a.hasZero || b.hasZero
+  hasPos  := a.hasPos  || b.hasPos
+  hasNeg  := a.hasNeg  || b.hasNeg
 
-  def neg(a: Sign): Sign := {
-    a with
-    hasNeg := a.hasPos
-    hasPos := a.hasNeg
-  }
+def meet(a b: Sign): Sign where
+  hasZero := a.hasZero && b.hasZero
+  hasPos  := a.hasPos  && b.hasPos
+  hasNeg  := a.hasNeg  && b.hasNeg
 
-  def sub(a b: Sign): Sign := a.add b.neg
-  def div(a b: Sign): Sign :=
-    if b = .Zero then
-      .None
-    else {
-      hasZero := a.hasZero,
-      hasPos  := a.hasPos && b.hasPos || a.hasNeg && b.hasNeg
-      hasNeg  := a.hasNeg && b.hasPos || b.hasNeg && b.hasPos
-
+def add(a b : Sign): Sign :=
+  if a = .None ∨ b = .None then
+    .None
+  else
+    {
+      hasZero := a.hasZero && b.hasZero ||
+                 a.hasNeg  && b.hasPos  ||
+                 a.hasPos  && b.hasNeg
+      -- Safe since we assume the other is non-empty
+      hasPos  := a.hasPos  || b.hasPos
+      -- Safe since we assume the other is non-empty
+      hasNeg  := a.hasNeg  || b.hasNeg
     }
 
-  def incl(a b: Sign): Bool :=
-    !(a.hasZero && !b.hasZero) &&
-    !(a.hasPos  && !b.hasPos ) &&
-    !(a.hasNeg  && !b.hasNeg )
+def mul(a b : Sign): Sign :=
+  if a = .Zero ∨ b = .Zero then
+    .Zero
+  else
+    {
+      hasZero := a.hasZero || b.hasZero,
+      hasPos  := a.hasPos && b.hasPos || a.hasNeg && b.hasNeg
+      hasNeg  := a.hasNeg && b.hasPos || b.hasNeg && b.hasPos
+    }
 
-  def join(a b: Sign): Sign where
-    hasZero := a.hasZero || b.hasZero
-    hasPos  := a.hasPos  || b.hasPos
-    hasNeg  := a.hasNeg  || b.hasNeg
+def neg(a: Sign): Sign := {
+  a with
+  hasNeg := a.hasPos
+  hasPos := a.hasNeg
+}
 
-  def meet(a b: Sign): Sign where
-    hasZero := a.hasZero && b.hasZero
-    hasPos  := a.hasPos  && b.hasPos
-    hasNeg  := a.hasNeg  && b.hasNeg
+def sub(a b: Sign): Sign :=
+  a.add b.neg
+
+def div(a b: Sign): Sign :=
+  if b = .Zero then
+    .None
+  else {
+    hasZero := a.hasZero,
+    hasPos  := a.hasPos && b.hasPos || a.hasNeg && b.hasNeg
+    hasNeg  := a.hasNeg && b.hasPos || b.hasNeg && b.hasPos
+  }
+
+def incl(a b: Sign): Bool :=
+  !(a.hasZero && !b.hasZero) &&
+  !(a.hasPos  && !b.hasPos ) &&
+  !(a.hasNeg  && !b.hasNeg )
 
 end Sign
 
@@ -129,8 +160,6 @@ section GaloisEmbedding
       simp [LE.le, Sign.incl] at *
       grind
 
-  attribute [local simp] Int.compare_eq_gt Int.compare_eq_lt
-
   open Classical in
   /--
     There is a Galois embedding between the Sign domain and the
@@ -144,12 +173,18 @@ section GaloisEmbedding
     our computations, just to justify the laws of operators.
   -/
   noncomputable instance instGESignIntSet: GaloisEmbedding (A := Sign) (C := Set Int) where
-    concrete a z := (z = 0 -> a.hasZero) ∧ (z > 0 -> a.hasPos) ∧ (z < 0 -> a.hasNeg)
+
+    concrete a z := match compare z 0 with
+      | .lt => a.hasNeg
+      | .eq => a.hasZero
+      | .gt => a.hasPos
+
     abstract X := {
       hasZero := X 0
       hasPos := ∃ z, z > 0 ∧ X z
       hasNeg := ∃ z, z < 0 ∧ X z
     }
+
     connection:= by
       rintro ⟨z,p,n⟩ X
       constructor
@@ -167,80 +202,79 @@ section GaloisEmbedding
         ·
           if h: ∃ x, 0 < x ∧ X x = true then
             obtain ⟨x, x_lt, Xx⟩ := h
-            specialize conc_lt x Xx; simp [x_lt] at conc_lt
-            obtain ⟨_,rfl,_⟩ := conc_lt
-            simp
+            specialize conc_lt x Xx; simp only [Int.compare_eq_gt.mpr x_lt] at conc_lt
+            grind [Ordering]
           else
             apply Or.inl
             simpa using h
         ·
           if h: ∃ x, x < 0 ∧ X x = true then
             obtain ⟨x, x_lt, Xx⟩ := h
-            specialize conc_lt x Xx; simp [x_lt] at conc_lt
-            obtain ⟨_,_,rfl⟩ := conc_lt
-            simp
+            specialize conc_lt x Xx; simp only [Int.compare_eq_lt.mpr x_lt] at conc_lt
+            grind
           else
             apply Or.inl
             simpa using h
+
     embedding := by
       rintro ⟨z,p,n⟩
       simp
       apply And.intro
       · cases p_def: p
         · simp; grind
-        · simp; exists 1; grind
+        · simp; exists 1
       · cases n_def: n
         · simp; grind
-        · simp; exists -1; grind
+        · simp; exists -1
+
 end GaloisEmbedding
 
-#check WidenLawful
 section theorems
 namespace Sign
 
-  theorem join_commutative(a b: Sign): a.join b = b.join a := by
-    have h: ∀ (a b: Sign), a.join b ≤ b.join a := by
-      intros a b
-      apply instGESignIntSet.lt_of_concrete_lt
-      intros x
-      simp [GaloisConnection.concrete, Sign.join]
-      grind
-    grind [Std.IsPartialOrder.le_antisymm]
-
-  theorem join_associative (a b c: Sign): (a.join b).join c = a.join (b.join c) := by
-    apply Std.IsPartialOrder.le_antisymm
-    all_goals(
-      apply instGESignIntSet.lt_of_concrete_lt
-      intros p
-      simp [GaloisConnection.concrete, Sign.join]
-      grind
-    )
-
-  theorem meet_commutative(a b: Sign): a.meet b = b.meet a := by
-    have h: ∀ (a b: Sign), a.meet b ≤ b.meet a := by
-      intros a b
-      apply instGESignIntSet.lt_of_concrete_lt
-      intros x
-      simp [GaloisConnection.concrete, Sign.meet]
-      grind
-    grind [Std.IsPartialOrder.le_antisymm]
-
-  theorem meet_associative(a b c: Sign): (a.meet b).meet c = a.meet (b.meet c) := by
-    apply Std.IsPartialOrder.le_antisymm
-    all_goals(
-      apply instGESignIntSet.lt_of_concrete_lt
-      intros p
-      simp [GaloisConnection.concrete, Sign.meet]
-      grind
-    )
-  theorem join_absorption: ∀ (x y: Sign), x.join (x.meet y) = x:= by
-    rintro ⟨z,p,n⟩ ⟨z',p',n'⟩
-    simp [Sign.meet, Sign.join]
+theorem join_commutative(a b: Sign): a.join b = b.join a := by
+  have h: ∀ (a b: Sign), a.join b ≤ b.join a := by
+    intros a b
+    apply instGESignIntSet.lt_of_concrete_lt
+    intros x
+    simp [GaloisConnection.concrete, Sign.join]
     grind
-  theorem meet_absorption: ∀ (x y: Sign), x.meet (x.join y) = x:= by
-    rintro ⟨z,p,n⟩ ⟨z',p',n'⟩
-    simp [Sign.meet, Sign.join]
+  grind [Std.IsPartialOrder.le_antisymm]
+
+theorem join_associative (a b c: Sign): (a.join b).join c = a.join (b.join c) := by
+  apply Std.IsPartialOrder.le_antisymm
+  all_goals(
+    apply instGESignIntSet.lt_of_concrete_lt
+    intros p
+    simp [GaloisConnection.concrete, Sign.join]
     grind
+  )
+
+theorem meet_commutative(a b: Sign): a.meet b = b.meet a := by
+  have h: ∀ (a b: Sign), a.meet b ≤ b.meet a := by
+    intros a b
+    apply instGESignIntSet.lt_of_concrete_lt
+    intros x
+    simp [GaloisConnection.concrete, Sign.meet]
+    grind
+  grind [Std.IsPartialOrder.le_antisymm]
+
+theorem meet_associative(a b c: Sign): (a.meet b).meet c = a.meet (b.meet c) := by
+  apply Std.IsPartialOrder.le_antisymm
+  all_goals(
+    apply instGESignIntSet.lt_of_concrete_lt
+    intros p
+    simp [GaloisConnection.concrete, Sign.meet]
+    grind
+  )
+theorem join_absorption: ∀ (x y: Sign), x.join (x.meet y) = x:= by
+  rintro ⟨z,p,n⟩ ⟨z',p',n'⟩
+  simp [Sign.meet, Sign.join]
+  grind
+theorem meet_absorption: ∀ (x y: Sign), x.meet (x.join y) = x:= by
+  rintro ⟨z,p,n⟩ ⟨z',p',n'⟩
+  simp [Sign.meet, Sign.join]
+  grind
 
 end Sign
 end theorems
@@ -312,7 +346,7 @@ instance: ValueDomain Sign where
   | .ge  => (x, y)
   | .gt  => (x, y)
 
-  rand
+  rand := fun
   | .some l, .some r =>
     if l ≤ r then
       {
@@ -334,3 +368,41 @@ instance: ValueDomain Sign where
       hasPos := false,
       hasNeg := l < 0
     }
+
+section Correctness
+
+open Classical in
+private noncomputable def Set.add(X Y: Set Int): Set Int
+:= λ z ↦ ∃ x y, z = x + y ∧ X x = true ∧ Y y = true
+
+theorem add_correct
+: instGESignIntSet.IsBinAbstraction Set.add Sign.add
+:= by
+  rintro ⟨z,p,n⟩ ⟨z',p',n'⟩
+  if h: ⟨z,p,n⟩ = Sign.None then
+    obtain ⟨rfl, rfl, rfl⟩ := h
+    intros e
+    simp [Sign.add, Sign.None, concrete, Set.add]
+    grind
+  else if h: ⟨z',p',n'⟩ = Sign.None then
+    obtain ⟨rfl, rfl, rfl⟩ := h
+    intros e
+    simp [Sign.add, Sign.None, concrete, Set.add]
+    grind
+  else
+  intros e
+  simp only [Set.add, Sign.add, decide_eq_true_eq]
+  rintro ⟨e1,e2, e1_e2, x_e1, y_e2⟩
+  cases com_e: compare e 0 <;> simp only [Int.compare_eq_eq, Int.compare_eq_lt, Int.compare_eq_gt] at com_e
+  all_goals (
+    simp only [ concrete ] at x_e1 y_e2 ⊢
+    grind
+  )
+  -- ADD IS NOT COMPLETE!
+  -- We have [<0] + [<0] = [<0]
+  #eval open Sign.Notation in
+    [<0] + [<0]
+  -- but if -1 ∈ γ ([<0] + [<0]) then it doesn't mean -1 ∈ (γ[<0] + γ[<0])).
+  -- In particular, -1 ∈ γ(a) + γ(b) → ∃ c ≥ 0 ∈ γ(a) ∪ γ(b), since -1 cannot
+  -- be obtained from the sum of two negative integers.
+end Correctness
