f50263cbb2f17d01f180c7fb635d40870afe62b4
arthur-adjedj <arthur.adjedj@gmail.com>
2025-12-11 15:41:15 +0100

    chore: roll back to v4.26.0-rc2, depend on mathlib The plan is to start using their existing infrastructure around Galois Connections instead of doing our own.

--- lakefile.lean
+++ lakefile.lean
@@ -1,7 +1,7 @@
 import Lake
 open Lake DSL
 
-require "leanprover-community" / "LeanSearchClient" @ git "3591c3f664ac3719c4c86e4483e21e228707bfa2"
+require "leanprover-community" / "mathlib"
 
 package "lustrean" where
   version := v!"0.1.0"
