c35af4a578a3f7620988e2431805e519d9a1046e
arthur-adjedj <arthur.adjedj@gmail.com>
2025-12-15 18:27:53 +0100

    chore: move to Lean 4.27.0-rc1

--- Test.lean
+++
@@ -1,6 +0,0 @@
-import Test.Reify
-import Test.Indicise
-import Test.Inline
-import Test.Normalize
-import Test.Compile
-import Test.Output
---
+++ Tests.lean
@@ -0,0 +1,6 @@
+import Tests.Reify
+import Tests.Indicise
+import Tests.Inline
+import Tests.Normalize
+import Tests.Compile
+import Tests.Output
