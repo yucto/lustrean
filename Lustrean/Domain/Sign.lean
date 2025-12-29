13e29f7afa5aeb38ca239c2564460f40fef56996
ayhon <ayhonnick@gmail.com>
2025-12-29 22:41:02 +0100

    feat: move simple tests to LustreanTest folder

--- Lustrean/Domain/Sign.lean
+++ Lustrean/Domain/Sign.lean
@@ -670,27 +670,3 @@ theorem rand_correctness (l? r?: Option Int)
 
 end Sign
 end Correctness
-
-section SimpleTests
-
-open Lustrean.Sign.Notation
-
-/-- info: [>0]-/
-#guard_msgs in #eval [>0] + [≥0]
-
-/-- info: [<0]-/
-#guard_msgs in #eval [<0] + [≤0]
-
-/-- info: [=0]-/
-#guard_msgs in #eval [<0] * [=0]
-
-/-- info: [⊥]-/
-#guard_msgs in #eval [<0] / [=0]
-
--- We keep 0 because if we take e ∈ γ[<0] and e' ∈ γ[≥0]
--- where e' > e.natAbs then e / e' = 0, since we are
--- performing division on integers
-/-- info: [≤0]-/
-#guard_msgs in #eval [<0] / [≥0]
-
-end SimpleTests
